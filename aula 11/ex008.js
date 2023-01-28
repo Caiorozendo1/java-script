9b9a14629a1ebec3a5129f340b8422d8d09f6583
Caiorozendo1 <rozendocaio360@gmail.com>
2023-01-28 13:36:35 -0300

    alguns exercicios de condiçoes simples

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+if(vel >= 60){
+    //condição simples
+    console.log('Vc foi multado!')
+}
+console.log(`dirija sempre com segurança`)
---
+++ aula 11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'USA'
+
+if (pais == 'brasil'){
+    console.log(`Você é Brasileiro!`)
+} else{
+    console.log('Você é estrangeiro!')
+}
