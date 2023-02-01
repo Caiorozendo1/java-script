5a9fdb7be4032fa2122a6edd4eb7367f25956609
Caiorozendo1 <rozendocaio360@gmail.com>
2023-02-01 19:27:28 -0300

    estruturas de repetiçoes

---
+++ aula 13/ambiente.js
@@ -0,0 +1,19 @@
+/*
+console.log('oi')
+console.log('oi')
+console.log('oi')
+console.log('oi')
+console.log('oi')
+console.log('oi')
+*/
+var c = 1
+/*
+while (c <= 6){
+    console.log(`Passo ${c}°`)
+    c++
+}
+*/
+do{
+    console.log(`passo ${c}°`)
+    c++
+}while(c <= 6)
